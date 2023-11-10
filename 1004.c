f4b04888f8f4d12ac01c0331f922bcd7203299b7
MD. MOINUL ISLAM <118617222+MOINUL909@users.noreply.github.com>
2023-11-10 11:23:04 +0600

    Add files via upload

---
+++ 1004.c
@@ -0,0 +1,17 @@
+/*MOINUL*/
+#include <stdio.h>
+
+int main() {
+    int n1,n2,PROD;
+    scanf("%d%d",&n1,&n2);
+    PROD=n1*n2;
+    printf("PROD = %d\n",PROD);
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+    return 0;
+}
